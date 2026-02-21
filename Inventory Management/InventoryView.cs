f273a8f73f1a83ce4989d7583a9f6570b3d1e878
Rey <reipiol42@gmail.com>
2026-02-21 10:21:43 +0800

    Create InventoryView.cs para makapass

---
+++ Inventory Management/InventoryView.cs
@@ -0,0 +1,83 @@
+using InventoryManagement.Services;
+
+namespace InventoryManagement.Views
+{
+    public class InventoryView
+    {
+        public InventoryService service;
+
+        public InventoryView()
+        {
+            service = new InventoryService();
+        }
+
+        public void Run()
+        {
+            bool running = true;
+
+            while (running)
+            {
+                Console.WriteLine("\n==== INVENTORY MENU ====");
+                Console.WriteLine("1. View Inventory");
+                Console.WriteLine("2. Update Stock");
+                Console.WriteLine("3. Reset Inventory");
+                Console.WriteLine("4. Exit");
+                Console.Write("Select option: ");
+
+                string choice = Console.ReadLine();
+
+                switch (choice)
+                {
+                    case "1":
+                        DisplayInventory();
+                        break;
+
+                    case "2":
+                        UpdateStock();
+                        break;
+
+                    case "3":
+                        service.ResetInventory();
+                        Console.WriteLine("Inventory has been reset.");
+                        break;
+
+                    case "4":
+                        running = false;
+                        Console.WriteLine("Exiting program...");
+                        break;
+
+                    default:
+                        Console.WriteLine("Invalid option.");
+                        break;
+                }
+            }
+        }
+
+        public void DisplayInventory()
+        {
+            string[,] inventory = service.GetInventory();
+
+            Console.WriteLine("\n--- Current Inventory ---");
+
+            for (int i = 0; i < inventory.GetLength(1); i++)
+            {
+                Console.WriteLine($"{i + 1}. {inventory[0, i]} - Stock: {inventory[1, i]}");
+            }
+        }
+
+        public void UpdateStock()
+        {
+            DisplayInventory();
+
+            Console.Write("Select product number to update: ");
+            int index = Convert.ToInt32(Console.ReadLine()) - 1;
+
+            Console.Write("Enter new stock quantity: ");
+            int newStock = Convert.ToInt32(Console.ReadLine());
+
+            service.UpdateStock(index, newStock);
+
+            Console.WriteLine("Stock updated successfully.");
+        }
+    }
+}
