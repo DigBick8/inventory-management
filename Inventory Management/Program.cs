b1cb3cec4315ad9f4121aca75876adda80844ec9
Rey <reipiol42@gmail.com>
2026-02-21 10:14:31 +0800

    Create Program.cs sa exam

---
+++ Inventory Management/Program.cs
@@ -0,0 +1,13 @@
+using InventoryManagement.Views;
+
+namespace InventoryManagement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            InventoryView start = new InventoryView();
+            start.Run();
+        }
+    }
+}
